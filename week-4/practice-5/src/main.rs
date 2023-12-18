4e57ddc6b01e7898c82b7c112150ff8e643c9e39
fishay.oCOS101 <shayowoseni@gmail.com>
2023-12-17 21:03:08 -0800

    (My first commit)

---
+++ week-4/practice-5/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+   let fullname = " Pan-atlantic University ";
+   println!();
+   println!("Name: {}", fullname);
+   println!("Before trim ");
+   println!("Length is {}",fullname.len());
+   println!();
+   println!("After trim ");
+   println!("Length is {}",fullname.trim().len());
+}
