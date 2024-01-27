965074becab805ff60bb5c4797d074f9fbdd7818
Owoseni Oluwafishayo <148012938+Fishay0@users.noreply.github.com>
2024-01-27 03:19:20 -0800

    Add files via upload

---
+++ globacom_db.sql
@@ -0,0 +1,65 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.1
+-- Dumped by pg_dump version 16.1
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: staff; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.staff (
+    staff_id integer NOT NULL,
+    staff_name text NOT NULL,
+    dno integer NOT NULL,
+    staff_sal real NOT NULL,
+    age integer NOT NULL,
+    mobile character varying(15) NOT NULL,
+    c_id integer,
+    c_name text,
+    c_age integer,
+    c_email character varying(15),
+    c_mobile character(15),
+    eid integer,
+    data_id integer
+);
+
+
+ALTER TABLE public.staff OWNER TO postgres;
+
+--
+-- Data for Name: staff; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.staff (staff_id, staff_name, dno, staff_sal, age, mobile, c_id, c_name, c_age, c_email, c_mobile, eid, data_id) FROM stdin;
+\.
+
+
+--
+-- Name: staff employees_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.staff
+    ADD CONSTRAINT employees_pkey PRIMARY KEY (staff_id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
